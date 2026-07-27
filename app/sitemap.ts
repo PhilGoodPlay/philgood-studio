4f73607d00ec2c1cb5385e013c27441471c92134
PhilGoodPlay <a.philippe.ch@icloud.com>
2026-07-27 09:20:57 +0200

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://www.philgoodstudio.ch,
+      url: "https://www.philgoodstudio.ch",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
